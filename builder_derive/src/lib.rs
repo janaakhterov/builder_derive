8f93fee30c82c89bbe3a64972e71c807c82470a6
Daniel Akhterov <akhterovd@gmail.com>
2018-12-23 19:53:27 -0800

    WIP: unzipping 4 tuple vec

--- builder_derive/src/lib.rs
+++ builder_derive/src/lib.rs
@@ -10,6 +10,9 @@ use syn::Fields;
 use proc_macro2::Ident;
 use proc_macro2::Span;
 use proc_macro2::TokenStream as TS;
+use failure::Error;
+use failure::err_msg;
+use std::result::Result;
 
 #[proc_macro_derive(Builder)]
 pub fn builder_derive(ast: TokenStream) -> TokenStream {
@@ -18,78 +21,83 @@ pub fn builder_derive(ast: TokenStream) -> TokenStream {
     impl_builder(&ast)
 }
 
-fn impl_builder_struct_fields(data: &DataStruct) -> TS {
-    let _fields = match &data.fields {
-        Fields::Named(fields) => {
-            fields.named.iter()
-                .map(|field| {
-                    // TODO: Not sure if clone is necessary
-                    let ident = &field.ident.clone().unwrap();
-                    let ty = &field.ty;
-                    quote!{ #ident: Option<#ty> }
-                }).collect::<Vec<TS>>()
-        },
-        _ => unimplemented!(),
-    };
+fn unzip(vec: Vec<(TS, TS, TS, TS)>) -> (Vec<TS>, Vec<TS>, Vec<TS>, Vec<TS>) {
+    let first: Vec<&TS> = Vec::new();
+    let second: Vec<&TS> = Vec::new();
+    let third: Vec<&TS> = Vec::new();
+    let forth: Vec<&TS> = Vec::new();
 
-    quote!{
-        #(#_fields),*
-    }
+    vec.iter().map(|(a, b, c , d)| {
+        first.push(a);
+        second.push(b);
+        third.push(c);
+        forth.push(d);
+    });
+
+    (first, second, third, forth)
 }
 
-fn impl_builder_struct_setters(data: &DataStruct) -> TS {
-    let _setters = match &data.fields {
+fn impl_builder_struct(data: &DataStruct, name: &Ident) -> TS {
+    let (_fields, 
+         _setters, 
+         _check_options, 
+         _idents_for_struct): (Vec<TS>, Vec<TS>, Vec<TS>, Vec<TS>) = match &data.fields {
         Fields::Named(fields) => {
-            fields.named.iter()
-                .map(|field| {
-                    // TODO: Not sure if clone is necessary
-                    let ident = &field.ident.clone().unwrap();
-                    let ty = &field.ty;
-                    let gen = quote! {
-                        fn #ident(&mut self, value: #ty) -> &mut Self {
-                            self.#ident = Some(value);
-                            self
-                        }
-                    };
+            unzip(fields.named.iter().map(|field| {
+                // TODO: Not sure if clone is necessary
+                let ident = &field.ident.clone().unwrap();
+                let ty = &field.ty;
 
-                    // panic!(gen.to_string());
-                    gen
-                }).collect::<Vec<TS>>()
+                let option_field = quote!{ #ident: Option<#ty> };
+
+                let setter = quote! {
+                    fn #ident(&mut self, value: #ty) -> &mut Self {
+                        self.#ident = Some(value);
+                        self
+                    }
+                };
+
+                let check_option = quote! {
+                    let #ident = self.#ident
+                        .expect("Builder wasn't completely initialized");
+                };
+
+                let ident_for_struct = quote!{ #ident };
+
+                (option_field, setter, check_option, ident_for_struct)
+            }).collect::<Vec<(TS, TS, TS, TS)>>())
         },
         _ => unimplemented!(),
     };
-
-    quote!{
-        #(#_setters)*
-    }
-}
-
-fn impl_builder(ast: &DeriveInput) -> TokenStream {
-    let (fields, setters) = match &ast.data {
-        Data::Struct(data) => {
-            (impl_builder_struct_fields(data),
-            impl_builder_struct_setters(data))
-        },
-        _ => unimplemented!(),
-    };
-
-    let name = format!("{}Builder", &ast.ident);
-    let name = Ident::new(&name, Span::call_site());
 
     let gen = quote! {
         #[derive(Default, Debug)]
         pub struct #name {
-            #fields
+            #(#_fields),*
         }
 
         impl #name {
-            fn build() {
-                println!("Hello, I'm a builder for TestBuilder");
-            }
+            #(#_setters)*
 
-            #setters
+            fn build(&self) -> Result<#name, Error> {
+                #(#_check_options)*
+
+                Ok(#name {
+                    #(#_idents_for_struct),*
+                })
+            }
         }
     };
 
-    gen.into()
+    gen
+}
+
+fn impl_builder(ast: &DeriveInput) -> TokenStream {
+    let name = format!("{}Builder", &ast.ident);
+    let name = Ident::new(&name, Span::call_site());
+
+    match &ast.data {
+        Data::Struct(data) => impl_builder_struct(data, &ast.ident).into(),
+        _ => unimplemented!(),
+    }
 }
--- builder_test/src/main.rs
+++ builder_test/src/main.rs
@@ -1,15 +1,20 @@
 use builder_derive::Builder;
+use failure::Error;
+use failure::err_msg;
+use std::result::Result;
 
 #[derive(Default, Builder)]
 struct Test {
     x: i64,
-    y: i64,
-    z: i64,
 }
+
 
 fn main() {
     println!("Hello, world!");
-    let mut builder = TestBuilder::default();
-    builder.y(10);
     println!("{:?}", TestBuilder::default());
+    let mut builder = TestBuilder::default();
+    builder.x(10);
+    let test = builder.build();
 }
+
+
